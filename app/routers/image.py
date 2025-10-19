dbf0dc215cd6336d7e44a7cc43296e5ced0f6ccb
Zoonhyeong <wkcpq35@naver.com>
2025-10-19 18:32:38 +0900

    image api blobname parameter 추가

--- app/routers/image.py
+++ app/routers/image.py
@@ -4,6 +4,14 @@ from app.services.upload_to_azure import upload_to_azure
 router = APIRouter(prefix="/api", tags=["image"])
 
 @router.post("/image")
-def upload_image(image: UploadFile = File(...)):
-    image_url = upload_to_azure(image)
+async def upload_image(image: UploadFile = File(...)):
+    if not image.filename:
+        return {"error": "File name is missing."}, 400
+
+    blob_name = image.filename
+
+    contents = await image.read()
+
+    image_url = upload_to_azure(data=contents, blob_name=blob_name, content_type=image.content_type)
+
     return {"imageUrl" : image_url}
